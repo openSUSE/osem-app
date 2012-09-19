87638098b25ec9a59ee4a143bbd0c808f43f846b
Matt Barringer <mbarringer@suse.de>
2012-09-19 22:17:24 +0200

    Adding licence header

--- client/src/de/incoherent/suseconferenceclient/adapters/PhoneScheduleAdapter.java
+++ client/src/de/incoherent/suseconferenceclient/adapters/PhoneScheduleAdapter.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Matt Barringer <matt@incoherent.de>.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     Matt Barringer <matt@incoherent.de> - initial API and implementation
+ ******************************************************************************/
 package de.incoherent.suseconferenceclient.adapters;
 
 import java.util.Calendar;
