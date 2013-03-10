4f46f61d7b47522c4de0552d77cf4e9ac28897ae
Nikolaos Chatzidakis <nikhatzi@gmail.com>
2013-03-10 16:55:51 +0200

    Updated conferences url - testing

--- client/src/de/incoherent/suseconferenceclient/Config.java
+++ client/src/de/incoherent/suseconferenceclient/Config.java
@@ -19,7 +19,7 @@ public class Config {
 	public static String MAPS_KEY = "04fPcVba6EsK5DoDqf4l6ZgTMoN7QHm9AuZpWhQ";
 	public static String TWITTER_KEY = "XYZ";
 	public static String PLUS_KEY = "AIzaSyAZvokqUnIi9BxpohDBCLsPc4bR8IK5mhw";
-	public static String BASE_URL = "http://incoherent.de/conferences";
+	public static String BASE_URL = "http://www.networklab.gr/conference";
 	// The level at which the map only displays the marker for the conference
 	// venue, and not food/drinks/etc.  The greater the number, the more
 	// zoomed in it is.
